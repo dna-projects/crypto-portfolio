3a9df08ea0fd927c30b147eca0b04975d906d5da
colord <10601293+colord@users.noreply.github.com>
2022-07-14 18:06:07 -0700

    Added all pages to templates, views, and routes part 1

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
-from .views import LoginPageView, MarketcapPageView
+from .views import LoginPageView, MarketcapBubblePageView, MarketcapPageView 
+from .views import MarketcapStatsPageView, PortfolioPageView, PortfolioStatsPageView, RegistrationPageView
 
 urlpatterns = [
+    # Entrypoints
+    path("login", LoginPageView.as_view(), name="login"),
+    path("registration", RegistrationPageView.as_view(), name="registration"),
+    # Portfolio related...
+    path("portfolio", PortfolioPageView.as_view(), name="portfolio"),
+    path("p-coin-stats", PortfolioStatsPageView.as_view(), name="p-coin-stats"),
+    # Marketcap related...
     path("mc", MarketcapPageView.as_view(), name="mc"),
-    path("login", LoginPageView.as_view(), name="login")
-    
+    path("mc-coin-stats", MarketcapStatsPageView.as_view(), name="mc-coin-stats"),
+    path("mc-bubble", MarketcapBubblePageView.as_view(), name="mc-bubble"),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,8 +1,27 @@
+from pstats import Stats
 from django.views.generic import TemplateView
 
-# Create your views here.
+# Entrypoints
 class LoginPageView(TemplateView):
     template_name = 'login.html'
 
+class RegistrationPageView(TemplateView):
+    template_name = 'registration.html'
+
+# Portfolio related...
+class PortfolioPageView(TemplateView):
+    template_name = 'portfolio.html'
+
+class PortfolioStatsPageView(TemplateView):
+    template_name = 'p-coin-stats.html'
+
+# Marketcap related...
+# TODO think about using ABC (abstract base classes)
 class MarketcapPageView(TemplateView):
     template_name = 'mc.html'
+
+class MarketcapStatsPageView(TemplateView):
+    template_name = 'mc-coin-stats.html'
+
+class MarketcapBubblePageView(TemplateView):
+    template_name = 'mc-bubble.html'
