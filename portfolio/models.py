113a29a349c1750eca3d3aa6ee7b5d782b701fd5
colord <10601293+colord@users.noreply.github.com>
2022-07-21 18:37:04 -0700

    Andrew and I worked on adding AssetEntry Model. Created superuser for admin tool. Integrated model into DB

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+from decimal import Decimal
 
 # Create your models here.
+
+# class User(models.model):
+#     pass
+
+class AssetEntry(models.Model):
+    name = models.CharField(max_length=24)
+    cost_basis = models.DecimalField(max_digits=12, decimal_places=2, validators=[MinValueValidator(Decimal('0.00'))])
+    price_at_purchase = models.DecimalField(max_digits=20, decimal_places=10, validators=[MinValueValidator(Decimal('0.0000000000'))])
+    quantity = models.DecimalField(max_digits=23, decimal_places=10, validators=[MinValueValidator(Decimal('0.0000000000'))])
+    entry_datetime = models.DateTimeField()
