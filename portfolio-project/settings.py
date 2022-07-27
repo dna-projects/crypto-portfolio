39b9e096c85ba61771bc88fd151a83ba95545a48
colord <10601293+colord@users.noreply.github.com>
2022-07-26 17:51:08 -0700

    Added user model. Added registration form. Updated registration template

--- portfolio-project/settings.py
+++ portfolio-project/settings.py
@@ -123,3 +123,5 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'portfolio.User'
