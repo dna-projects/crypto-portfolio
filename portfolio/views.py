edda7639d9c613e37ca2833d987d60187f9022d1
Andrew Finsand <andfinsand@gmail.com>
2022-08-04 18:35:14 -0700

    Attempted 3 PortfolioPageViews.

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,6 +1,5 @@
 from pstats import Stats
-from django.views.generic import TemplateView, CreateView
-from django.views.generic.edit import UpdateView
+from django.views.generic import TemplateView, CreateView, View, FormView
 from portfolio.forms import UserRegistrationForm
 from portfolio.forms import NewTokenForm
 from portfolio.models import AssetEntry
@@ -17,14 +16,40 @@ class RegistrationPageView(CreateView):
     success_url = reverse_lazy('login')
 
 # Portfolio related...
-class PortfolioPageView(TemplateView):
+class PortfolioPageView(CreateView):
     template_name = 'portfolio.html'
     form_class = NewTokenForm
-    asset_entries = AssetEntry.objects.all()
     success_url = reverse_lazy('portfolio')
 
-    # def get(self, request):
-    #     return render(request, self.template_name, {'asset_entries': self.asset_entries})
+    # TODO - Last update: 8/4/22
+    # Having issues with showing the form AND showing the asset entries at the same time
+    # 
+    # Our custom get request needs to:
+    # 1. Grab the asset entries
+    # 2. Grab the form fields (I'm thinking this is why it's missing from the html)
+    #   a. Manually coding the get/post request for a form
+    def get(self, request, *args, **kwargs):
+        print('Get request successful')
+        view = PortfolioPageTemplateView.as_view()
+        return view(request, *args, **kwargs)
+
+    # def post(self, request, *args, **kwargs):
+    #     print('Post request successful')
+    #     view = PortfolioPageCreateView.as_view()
+    #     return view(request, *args, **kwargs)
+
+class PortfolioPageTemplateView(TemplateView):
+    template_name = 'portfolio.html'
+    asset_entries = AssetEntry.objects.all()
+
+    def get(self, request):
+        return render(request, self.template_name, {'asset_entries': self.asset_entries})
+
+# Form
+class PortfolioPageCreateView(FormView):
+    template_name = 'portfolio.html'
+    form_class = NewTokenForm
+    success_url = reverse_lazy('portfolio')
 
 class PortfolioStatsPageView(TemplateView):
     template_name = 'p-coin-stats.html'
