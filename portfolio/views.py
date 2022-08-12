83f01909ed61eec37cbe98b53b11b967699fc970
Andrew Finsand <andfinsand@gmail.com>
2022-08-12 01:49:00 -0700

    Data class for token information added to API.
    Co-authored-by: colord <colord@users.noreply.github.com>

--- portfolio/views.py
+++ portfolio/views.py
@@ -6,6 +6,7 @@ from portfolio.forms import NewTokenForm
 from portfolio.models import AssetEntry
 from django.shortcuts import render
 from django.urls import reverse_lazy
+from dataclasses import dataclass
 import requests 
 import json
 
@@ -39,16 +40,56 @@ class PortfolioStatsPageView(TemplateView):
 class MarketcapPageView(TemplateView):
     template_name = 'mc.html'
     
-    def testAPI(self, request):
+    @dataclass
+    class TokenAPI:
+        name: str
+        symbol: str
+        rank: int
+        price: float
+        one_hour_performance: float
+        one_day_performance: float
+        seven_day_performance: float
+        one_day_volume: int
+        marketcap: int
+        
+        def __init__(self, name):
+            self.name = name
+            response = requests.get(self.build_request())
+            asset = json.loads(response.content)
+            self.symbol = asset['symbol']
+            self.rank = asset['market_cap_rank']
+            self.price = asset['market_data']['current_price']['usd']
+
+            # TODO need to find one hour performance
+            # self.one_hour_performance = asset['market_data']['']
+            self.one_day_performance = asset['market_data']['price_change_percentage_24h']
+            self.seven_day_performance = asset['market_data']['price_change_percentage_7d']
+            self.one_day_volume = asset['market_data']['total_volume']['usd']
+            # TODO need to find 24h volume
+            self.marketcap = asset['market_data']['market_cap']['usd']
+
+        def build_request(self):
+            return f"https://api.coingecko.com/api/v3/coins/{self.name}"
+
+    def get(self, request):
         # token_name = {'Ethereum': 'ethereum', 'Bitcoin': 'bitcoin'}
         # https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=USD
         # f"https://api.coingecko.com/api/v3/simple/price?ids={token_name}&vs_currencies=USD
-        response = requests.get("")
-        asset = json.loads(response.content)
-        asset_name = asset['_name']
-        # asset_name = 'coin gecko request for BTC coin name'
-        asset_price = 'coin gecko request for BTC'
-        return render(request, self.template_name, {'asset_name': asset_name, 'asset_price': asset_price})
+        
+        # coin_gecko_list = ["ether", 'btc', 'solana']
+
+        # token_list = []
+        # for token_names in coin_gecko_list:
+        #     token_list.append(self.TokenAPI(name=token_names))
+
+        token_list = [
+            self.TokenAPI(name="ethereum"),
+            self.TokenAPI(name="bitcoin"),
+            self.TokenAPI(name="solana")
+        ]
+        # # asset_name = 'coin gecko request for BTC coin name'
+        # asset_price = 'coin gecko request for BTC'
+        return render(request, self.template_name, {'token_list': token_list})
 
 class MarketcapStatsPageView(TemplateView):
     template_name = 'mc-coin-stats.html'
