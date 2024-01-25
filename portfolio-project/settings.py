964c502fcd78b3754f004e90e6a886fc06cd7e84
Andrew Finsand <andfinsand@gmail.com>
2024-01-25 12:55:52 -0800

    Revert back to API request in template for search feature. Kept validate function to ensure no spaces as first character. Kept debounce function for delay before running API as well as minimum character limit of 2. Combined validate and CoinGecko search.

--- portfolio-project/settings.py
+++ portfolio-project/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+
+CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1']
 
 
 # Application definition
