7922dda8995c3d370c23052d38e9673020d71afb
Andrew Finsand <andfinsand@gmail.com>
2024-02-07 18:38:44 -0800

    Update to static url.

--- portfolio-project/settings.py
+++ portfolio-project/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'staticfiles/'
+STATIC_URL = 'backend/staticfiles/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
